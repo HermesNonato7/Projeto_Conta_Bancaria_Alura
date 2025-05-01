a578fb5278da29bb5bd8a79d21a4c866924df292
HermesNonato8 <hermesnonato8@hotail.com>
2025-04-30 21:55:19 -0300

    add inicio do codigo

--- src/Desafio.java
+++ src/Desafio.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Desafio {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        String nome = "Hermes";
+        String tipoConta = "Corrente";
+        double saldo = 1599.99;
+        int opcao = 0;
+
+        System.out.println("*******************************");
+        System.out.println("\nNome do cliente: " + nome);
+        System.out.println("Tipo da conta: " + tipoConta);
+        System.out.println("Saldo atual: " + saldo);
+        System.out.println("\n*******************************");
+
+        String menu = """
+                ** Digite sua opção **
+                1 - Consultar saldo
+                2 - Depositar valor
+                3 - Sacar valor
+                4 - Sair
+                
+                """;
+
+        Scanner leitura = new Scanner(System.in);
+
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+
+        }
+
     }
 }
